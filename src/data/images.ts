3b17ec3a002b73bf5500677c3cfcad6cf27f2afd
Pete Watson-Wailes <pete@watson-wail.es>
2025-01-25 08:18:57 +0000

    updating image media

--- src/data/images.ts
+++ src/data/images.ts
@@ -21,20 +21,34 @@ import ceresZ3 from '../assets/imgs/galleries/ceres/z3.png'
 import ceresZ4 from '../assets/imgs/galleries/ceres/z4.png'
 import ceresZ5 from '../assets/imgs/galleries/ceres/z5.png'
 
-import shipZ1 from '../assets/imgs/galleries/ship/z1.png'
-import shipZ2 from '../assets/imgs/galleries/ship/z2.png'
-import shipZ3 from '../assets/imgs/galleries/ship/z3.png'
-import shipZ4 from '../assets/imgs/galleries/ship/z4.png'
+import ceresDocks from '../assets/imgs/scenes/ceres/docks.png'
+import littleHygeia from '../assets/imgs/scenes/ceres/little-hygeia.png'
+import minersMile from '../assets/imgs/scenes/ceres/miners-mile.png'
+import theDrum from '../assets/imgs/scenes/ceres/the-drum.png'
+import thePit from '../assets/imgs/scenes/ceres/the-pit.png'
 
-import ceres from '../assets/imgs/scenes/ceres/ceres.png'
-import pallas from '../assets/imgs/scenes/pallas/pallas.png'
-import vesta from '../assets/imgs/scenes/vesta/vesta.png'
+import botanist from '../assets/imgs/scenes/hygeia/botanist.png'
+import hygeiaDocks from '../assets/imgs/scenes/hygeia/docks.png'
+import farm from '../assets/imgs/scenes/hygeia/farm.png'
 import hygeia from '../assets/imgs/scenes/hygeia/hygeia.png'
+import mainIsle from '../assets/imgs/scenes/hygeia/main-isle.png'
 
-import space1 from '../assets/imgs/backgrounds/space_1.png'
-import space2 from '../assets/imgs/backgrounds/space_2.png'
-import space3 from '../assets/imgs/backgrounds/space_3.png'
-import space4 from '../assets/imgs/backgrounds/space_4.png'
+import officeScene from '../assets/imgs/scenes/pallas/office-scene.png'
+import pallas from '../assets/imgs/scenes/pallas/pallas.png'
+import steelsBar from '../assets/imgs/scenes/pallas/steels-bar.png'
+import streetScene from '../assets/imgs/scenes/pallas/street-scene.png'
+
+import interior3 from '../assets/imgs/scenes/ships/interior_3.png'
+import interior5 from '../assets/imgs/scenes/ships/interior_5.png'
+import prospector from '../assets/imgs/scenes/ships/prospector.png'
+import warDog from '../assets/imgs/scenes/ships/war-dog.png'
+
+import battleships from '../assets/imgs/scenes/vesta/battleships.png'
+import factory from '../assets/imgs/scenes/vesta/factory.png'
+import robotz from '../assets/imgs/scenes/vesta/robotz.png'
+import ruins from '../assets/imgs/scenes/vesta/ruins.png'
+import scienceLab from '../assets/imgs/scenes/vesta/science-lab.png'
+import vesta from '../assets/imgs/scenes/vesta/vesta.png'
 
 
 export default {
@@ -53,16 +67,28 @@ export default {
   lucas,
   sadie,
   shehzada,
-  shipZ1,
-  shipZ2,
-  shipZ3,
-  shipZ4,
-  space1,
-  space2,
-  space3,
-  space4,
-  ceres,
+  ceresDocks,
+  littleHygeia,
+  minersMile,
+  theDrum,
+  thePit,
+  botanist,
+  hygeiaDocks,
+  farm,
+  hygeia,
+  mainIsle,
+  officeScene,
   pallas,
+  steelsBar,
+  street,
+  interior3,
+  interior5,
+  prospector,
+  warDog,
+  battleships,
+  factory,
+  robotz,
+  ruins,
+  scienceLab,
   vesta,
-  hygeia
 }
