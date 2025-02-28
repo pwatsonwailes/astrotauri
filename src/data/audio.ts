e8a28d562ae804c5c41489588d85f8dfa42b9166
pwatsonwailes <pete@watson-wail.es>
2025-02-28 11:15:51 +0000

    updating audio system

---
+++ src/data/audio.ts
@@ -0,0 +1,11 @@
+import continueSfx from '../assets/sfx/continue.mp3';
+import completeStorySfx from '../assets/sfx/complete_story.mp3';
+import notificationSfx from '../assets/sfx/notification.mp3';
+import zingSfx from '../assets/sfx/zing.mp3';
+
+export default {
+  continueSfx,
+  completeStorySfx,
+  notificationSfx,
+  zingSfx
+}
--- src/hooks/useSoundSystem.ts
+++ src/hooks/useSoundSystem.ts
@@ -1,13 +1,15 @@
 import { useCallback } from 'react';
 
+import audio from '../data/audio';
+
 // Sound effects from Pixabay (free for commercial use)
 const SOUND_EFFECTS = {
-  navigation: new Audio('https://cdn.pixabay.com/download/audio/2022/03/24/audio_c8c8a73467.mp3?filename=click-124467.mp3'),
-  continue: new Audio('https://cdn.pixabay.com/download/audio/2022/03/19/audio_c8c8b2b807.mp3?filename=click-124472.mp3'),
-  choice: new Audio('https://cdn.pixabay.com/download/audio/2022/03/15/audio_c8c8b2b808.mp3?filename=click-124476.mp3'),
-  complete: new Audio('https://cdn.pixabay.com/download/audio/2021/08/04/audio_c8c8b2b809.mp3?filename=success-1-6297.mp3'),
-  newGame: new Audio('https://cdn.pixabay.com/download/audio/2021/08/09/audio_88347d37c9.mp3?filename=success-fanfare-trumpets-6185.mp3'),
-  select: new Audio('https://cdn.pixabay.com/download/audio/2022/03/22/audio_c8c8a73469.mp3?filename=click-124469.mp3')
+  navigation: new Audio(audio.zingSfx),
+  continue: new Audio(audio.continueSfx),
+  choice: new Audio(audio.continueSfx),
+  complete: new Audio(audio.completeStorySfx),
+  newGame: new Audio(audio.notificationSfx),
+  select: new Audio(audio.continueSfx)
 };
 
 // Preload all audio
